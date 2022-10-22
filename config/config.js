e2659aa154a62550ee701cd9b551a132845e2adb
boytalaba <36549438+boytalaba@users.noreply.github.com>
2022-10-22 07:53:54 -0700

    Add files via upload

---
+++ config/config.js
@@ -0,0 +1,20 @@
+const config = {
+    app_name: 'CRAPrity',
+    app_description: 'CRAPRarity is an open source package for easy rarity score calculation with ERC721 NFT metadata collection.',
+    collection_file_name: 'collection.json',
+    collection_contract_address: '',
+    collection_name: 'CRAPtos',
+    collection_description: 'A collection of 1 111 degen NFT CRAPS! All are unique and randomly generated with rich pfpfffttsss... blaaaattt....prrrrt.. diverse rarity on APTOS BLOCKCHAIN',
+    collection_id_from: 0,
+    ignore_traits: ['date'], 
+    sqlite_file_name: 'database.sqlite',
+    ga: 'G-BW69Z04YTP',
+    main_og_image: '',
+    item_path_name: 'CRAP',
+    page_item_num: 60,
+    content_image_is_video: false,
+    content_image_frame: 'circle', // circle, rectangle
+    use_wallet: false
+};
+
+module.exports = config;
